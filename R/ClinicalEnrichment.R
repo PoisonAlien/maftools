5b7b729d5c6f5e8e0ad1f8f7c02e28bc03805f88
PoisonAlien <anandmt3@gmail.com>
2018-01-26 14:10:06 +0100

    add clinicalEnrichment

---
+++ R/ClinicalEnrichment.R
@@ -0,0 +1,112 @@
+#' Perform mutational enrichment analysis for a given clinical feature (MAF).
+#' @details Performs paiwise and groupwise fisher exact tests to find differentially enriched genes for every factor within a clinical feature.
+#'
+#' @param maf \code{\link{MAF}} object
+#' @param clinicalFeature columns names from `clinical.data` slot of \code{MAF} to be analysed for.
+#' @param minMut Consider only genes with minimum this number of samples mutated. Default 5.
+#' @param useCNV whether to include copy number events. Only applicable when MAF is read along with copy number data. Default TRUE if available.
+#' @return result list containing p-values
+#' @examples
+#' laml.maf = system.file('extdata', 'tcga_laml.maf.gz', package = 'maftools')
+#' laml.clin = system.file('extdata', 'tcga_laml_annot.tsv', package = 'maftools')
+#' laml = read.maf(maf = laml.maf, clinicalData = laml.clin)
+#' clinicalEnrichment(laml, FAB_classification)
+
+
+
+clinicalEnrichment = function(maf, clinicalFeature = NULL, minMut = 5, useCNV = TRUE){
+
+  if(is.null(clinicalFeature)){
+    stop("Missing clinicalFeature. Use getClinicalData() to see available features.")
+  }
+
+  cd = getClinicalData(x = maf)[,c("Tumor_Sample_Barcode", clinicalFeature), with = FALSE]
+  colnames(cd)[2] = 'cf'
+  cf.tbl = table(cd$cf)
+  message(paste0("Sample size per factor in ", clinicalFeature, ":"))
+  print(cf.tbl)
+
+  if(length(cf.tbl) == 1){
+    stop("Single factor. Nothing to compare..")
+  }
+
+  #Source code from reporttools (https://github.com/cran/reporttools/blob/master/R/pairwise.fisher.test.r)
+  pairwise.fisher.test <- function(x, g, p.adjust.method, ...){
+    DNAME <- paste(deparse(substitute(x)), "and", deparse(substitute(g)))
+    g <- factor(g)
+
+    compare.levels <- function(i, j) {
+      ind <- (as.integer(g) %in% c(i, j)) & (is.na(x) == FALSE) & (is.na(g) == FALSE)
+      xi <- factor(x[ind], exclude = NULL)
+      xj <- factor(g[ind], exclude = NULL)
+      tab <- table(xi, xj)
+      nonzeromarginal <- (min(apply(tab, 1, sum)) * min(apply(tab, 2, sum)) > 0)
+      size <- ((nrow(tab) > 1) * (ncol(tab) > 1) > 0)
+      if ((nonzeromarginal == TRUE) & (size == TRUE)){fisher.test(xi, xj, ...)$p.value} else {NA}
+    }
+
+    PVAL <- pairwise.table(compare.levels, levels(g), p.adjust.method)
+    ans <- list(data.name = DNAME, p.value = PVAL, p.adjust.method = p.adjust.method)
+    class(ans) <- "pairwise.htest"
+    return(ans)
+  }
+
+  if(useCNV){
+    genes = getGeneSummary(x = maf)[AlteredSamples > minMut, Hugo_Symbol]
+  }else{
+    genes = getGeneSummary(x = maf)[MutatedSamples > minMut, Hugo_Symbol]
+  }
+
+  plist = lapply(genes, function(x){
+          g = unique(genesToBarcodes(maf = maf, genes = x, justNames = TRUE)[[1]])
+          cd$Genotype = ifelse(test = cd$Tumor_Sample_Barcode %in% g, yes = "Mutant", no = "WT")
+
+          #Perform groupwise comparision for given gene
+          ft = lapply(X = names(cf.tbl), FUN = function(y){
+            cd$Group = ifelse(test = cd$cf %in% y, yes = y, no = "Other")
+            ft = fisher.test(x = with(cd, table(Genotype, Group)))
+            ft.tbl = data.table::data.table(Group1 = y, Group2 = "Rest",
+                                            n_mutated_group1 = paste0(nrow(cd[Group %in% y][Genotype %in% 'Mutant']), " of ", nrow(cd[Group %in% y])),
+                                            n_mutated_group2 = paste0(nrow(cd[!Group %in% y][Genotype %in% 'Mutant']), " of ", nrow(cd[!Group %in% y])),
+                                            p_value = ft$p.value, OR_low = ft$conf.int[1], OR_high = ft$conf.int[2],
+                                            Hugo_Symbol = x, Analysis = "Group")
+            ft.tbl
+          })
+          ft = data.table::rbindlist(ft)
+
+          #Perform pairwise fisher test for every gene
+          prop.tbl = pairwise.fisher.test(x = cd$Genotype, g = cd$cf, p.adjust.method = "fdr")
+          ptbl = data.table::melt(prop.tbl$p.value)
+          data.table::setDT(ptbl)
+          ptbl[,Hugo_Symbol := x][,Analysis := "Pairwise"]
+          ptbl = ptbl[,.(Hugo_Symbol, Var1, Var2, value, Analysis)]
+          colnames(ptbl) = c("Hugo_Symbol", "Feature_1", "Feature_2", "fdr", "Analysis")
+          ptbl = ptbl[!is.na(fdr)]
+
+          f1.mutants = cd[,.N,.(cf, Genotype)][Genotype %in% 'Mutant', .(cf, N)]
+          if(length(names(cf.tbl)[!names(cf.tbl) %in% f1.mutants[,cf]]) > 0){
+            #Add zero counts for missing factors
+            f1.mutants = rbind(f1.mutants,
+                               data.table::data.table(cf = names(cf.tbl)[!names(cf.tbl) %in% f1.mutants[,cf]], N = 0))
+          }
+
+          f1.mutants = merge(f1.mutants, cd[,.N,.(cf)], by = 'cf')
+          f1.mutants[,n_mutated_Feature := paste0(N.x, " of ", N.y)]
+
+          ptbl = merge(ptbl, f1.mutants[,.(cf, n_mutated_Feature)], by.x = 'Feature_1', by.y = 'cf', all.x = TRUE)
+          ptbl = merge(ptbl, f1.mutants[,.(cf, n_mutated_Feature)], by.x = 'Feature_2', by.y = 'cf', all.x = TRUE)
+          colnames(ptbl)[6:7] = c('n_mutated_Feature1', 'n_mutated_Feature2')
+          ptbl = ptbl[,.(Hugo_Symbol, Feature_1, Feature_2, n_mutated_Feature1, n_mutated_Feature2, fdr, Analysis)]
+
+          ptbl = rbind(ptbl, ft, fill = TRUE)
+          ptbl
+        })
+
+  plist = data.table::rbindlist(l = plist, fill = TRUE)
+
+  pw.pvals = plist[Analysis %in% "Pairwise",.(Hugo_Symbol, Feature_1, Feature_2, n_mutated_Feature1, n_mutated_Feature2, fdr)][order(fdr)]
+  gw.pvals = plist[Analysis %in% "Group",.(Hugo_Symbol, Group1, Group2, n_mutated_group1, n_mutated_group2, p_value, OR_low, OR_high)][order(p_value)]
+  gw.pvals[,fdr := p.adjust(p_value, method = "fdr")]
+
+  return(list(pairwise_comparision = pw.pvals, groupwise_comparision = gw.pvals, cf_sizes = cd[,.N,cf]))
+}
--- R/plotSignatures.R
+++ R/plotSignatures.R
@@ -13,78 +13,78 @@
 #'
 plotSignatures = function(nmfRes = NULL, contributions = FALSE, color = NULL, patient_order = NULL, ...){
 
-    conv.mat.nmf.signatures = nmfRes$signatures
-    contrib = nmfRes$contributions
-    coSineMat = nmfRes$coSineSimMat
+  conv.mat.nmf.signatures = nmfRes$signatures
+  contrib = nmfRes$contributions
+  coSineMat = nmfRes$coSineSimMat
 
-    if(contributions){
-      contribt = t(contrib)
-      #calculate sd
-      if(!is.null(patient_order)){
-            contribt = contribt[patient_order,] #order on user-specified ordering of the genomes
-        }else{
-        contribt = contribt[order(contribt[,ncol(contribt)]),] #order according to standard deviation
-        }
-
-      #contrib = t(contribt[,1:(ncol(contribt)-1)])
-      contrib = t(contribt[,1:(ncol(contribt))])
-      contrib.melt = data.table::melt(contrib)
-
-      contrib.gg = ggplot(data = contrib.melt, aes(x = Var2, y = value, fill = Var1))+geom_bar(stat = 'identity')+
-        ylab('Fraction signature contribution')+xlab('Sample')+cowplot::theme_cowplot(line_size = 1)+
-        theme(legend.position = 'bottom', axis.ticks = element_blank(), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-              legend.title = element_blank())+xlab("Samples")
-
-
-      return(contrib.gg)
-
+  if(contributions){
+    contribt = t(contrib)
+    #calculate sd
+    if(!is.null(patient_order)){
+      contribt = contribt[patient_order,] #order on user-specified ordering of the genomes
     }else{
-      aetiology = structure(list(aetiology = c("spontaneous deamination of 5-methylcytosine",
-                                               "APOBEC Cytidine Deaminase (C>T)", "defects in DNA-DSB repair by HR",
-                                               "exposure to tobacco (smoking) mutagens", "Unknown", "defective DNA mismatch repair",
-                                               "UV exposure", "Unknown", "defects in polymerase-eta", "defects in polymerase POLE",
-                                               "exposure to alkylating agents", "Unknown", "APOBEC Cytidine Deaminase (C>G)",
-                                               "Unknown", "defective DNA mismatch repair", "Unknown", "Unknown",
-                                               "Unknown", "Unknown", "defective DNA mismatch repair", "unknown",
-                                               "exposure to aristolochic acid", "Unknown", "exposures to aflatoxin",
-                                               "Unknown", "defective DNA mismatch repair", "Unknown", "Unknown",
-                                               "exposure to tobacco (chewing) mutagens", "Unknown")), .Names = "aetiology", row.names = c("Signature_1",
-                                                                                                                                          "Signature_2", "Signature_3", "Signature_4", "Signature_5", "Signature_6",
-                                                                                                                                          "Signature_7", "Signature_8", "Signature_9", "Signature_10",
-                                                                                                                                          "Signature_11", "Signature_12", "Signature_13", "Signature_14",
-                                                                                                                                          "Signature_15", "Signature_16", "Signature_17", "Signature_18",
-                                                                                                                                          "Signature_19", "Signature_20", "Signature_21", "Signature_22",
-                                                                                                                                          "Signature_23", "Signature_24", "Signature_25", "Signature_26",
-                                                                                                                                          "Signature_27", "Signature_28", "Signature_29", "Signature_30"
-                                               ), class = "data.frame")
-
-      plotData = as.data.frame(t(conv.mat.nmf.signatures))
-      nsigs = nrow(plotData)
-
-      if(is.null(color)){
-        #color = c("blue","black","red","gray","green","maroon")
-        color = c('coral4', 'lightcyan4', 'deeppink3', 'lightsalmon1', 'forestgreen', 'cornflowerblue')
-      }
-      colors = rep(color, each=16)
-
-      par(mfrow = c(nsigs,1),oma = c(5,4,0,0) + 0.1,mar = c(0,0,1,1) + 0.1, las=1, tcl=-.25, font.main=4, xpd = NA)
-
-
-      for(i in 1:nsigs){
-        ae.sig = names(which(coSineMat[i,] == max(coSineMat[i,])))
-        ae = as.character(aetiology[ae.sig,])
-        #ae = paste0("Aetiology: ", ae, " \n cosine-similarity: ", max(coSineMat[i,]))
-        ae = paste0(ae.sig, " like; cosine-similarity: ", round(max(coSineMat[i,]), digits = 3), " \n Aetiology: ", ae)
-        d = as.matrix(plotData[i,])
-        barplot(d, xaxt = "n", yaxt = "n", col = colors, beside = TRUE, ylim = c(-0.1, 0.3),
-                cex.main = 1, border = NA, font.axis = 2, font.lab = 2,
-                adj = 0.25, ...)
-        title(main = ae, cex.main=0.9, line = -0.3)
-        #mtext(text = ae, side = 1, line = 2, font = 1, cex = 0.5, at = 0.3)
-        axis(side = 2,at = seq(0, 0.3, 0.1),labels = seq(0, 0.3, 0.1), pos = seq(0, 0.3, 0.1), las = 2, lwd = 2, hadj = 0.4)
-        #abline(h = seq(0, 0.3, 0.1),lty=2,lwd=0.3, col = 'gray70')
-        rect(xleft = seq(0, 192, 32), ybottom = -0.05, xright = 192, ytop = -0.02, col = color, border = 'gray70')
-        text(labels = c("C>A","C>G","C>T","T>A","T>C","T>G"),y = rep(-0.08,6),x = seq(0, 192, 32)[2:7]-16, cex = 1)
-      }
+      contribt = contribt[order(contribt[,ncol(contribt)]),] #order according to standard deviation
     }
+
+    #contrib = t(contribt[,1:(ncol(contribt)-1)])
+    contrib = t(contribt[,1:(ncol(contribt))])
+    contrib.melt = data.table::melt(contrib)
+
+    contrib.gg = ggplot(data = contrib.melt, aes(x = Var2, y = value, fill = Var1))+geom_bar(stat = 'identity')+
+      ylab('Fraction signature contribution')+xlab('Sample')+cowplot::theme_cowplot(line_size = 1)+
+      theme(legend.position = 'bottom', axis.ticks = element_blank(), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+            legend.title = element_blank())+xlab("Samples")
+
+
+    return(contrib.gg)
+
+  }else{
+    aetiology = structure(list(aetiology = c("spontaneous deamination of 5-methylcytosine",
+                                             "APOBEC Cytidine Deaminase (C>T)", "defects in DNA-DSB repair by HR",
+                                             "exposure to tobacco (smoking) mutagens", "Unknown", "defective DNA mismatch repair",
+                                             "UV exposure", "Unknown", "defects in polymerase-eta", "defects in polymerase POLE",
+                                             "exposure to alkylating agents", "Unknown", "APOBEC Cytidine Deaminase (C>G)",
+                                             "Unknown", "defective DNA mismatch repair", "Unknown", "Unknown",
+                                             "Unknown", "Unknown", "defective DNA mismatch repair", "unknown",
+                                             "exposure to aristolochic acid", "Unknown", "exposures to aflatoxin",
+                                             "Unknown", "defective DNA mismatch repair", "Unknown", "Unknown",
+                                             "exposure to tobacco (chewing) mutagens", "Unknown")), .Names = "aetiology", row.names = c("Signature_1",
+                                                                                                                                        "Signature_2", "Signature_3", "Signature_4", "Signature_5", "Signature_6",
+                                                                                                                                        "Signature_7", "Signature_8", "Signature_9", "Signature_10",
+                                                                                                                                        "Signature_11", "Signature_12", "Signature_13", "Signature_14",
+                                                                                                                                        "Signature_15", "Signature_16", "Signature_17", "Signature_18",
+                                                                                                                                        "Signature_19", "Signature_20", "Signature_21", "Signature_22",
+                                                                                                                                        "Signature_23", "Signature_24", "Signature_25", "Signature_26",
+                                                                                                                                        "Signature_27", "Signature_28", "Signature_29", "Signature_30"
+                                             ), class = "data.frame")
+
+    plotData = as.data.frame(t(conv.mat.nmf.signatures))
+    nsigs = nrow(plotData)
+
+    if(is.null(color)){
+      #color = c("blue","black","red","gray","green","maroon")
+      color = c('coral4', 'lightcyan4', 'deeppink3', 'lightsalmon1', 'forestgreen', 'cornflowerblue')
+    }
+    colors = rep(color, each=16)
+
+    par(mfrow = c(nsigs,1),oma = c(5,4,0,0) + 0.1,mar = c(0,0,1,1) + 0.1, las=1, tcl=-.25, font.main=4, xpd = NA)
+
+
+    for(i in 1:nsigs){
+      ae.sig = names(which(coSineMat[i,] == max(coSineMat[i,])))
+      ae = as.character(aetiology[ae.sig,])
+      #ae = paste0("Aetiology: ", ae, " \n cosine-similarity: ", max(coSineMat[i,]))
+      ae = paste0(ae.sig, " like; cosine-similarity: ", round(max(coSineMat[i,]), digits = 3), " \n Aetiology: ", ae)
+      d = as.matrix(plotData[i,])
+      barplot(d, xaxt = "n", yaxt = "n", col = colors, beside = TRUE, ylim = c(-0.1, 0.3),
+              cex.main = 1, border = NA, font.axis = 2, font.lab = 2,
+              adj = 0.25, ...)
+      title(main = ae, cex.main=0.9, line = -0.3)
+      #mtext(text = ae, side = 1, line = 2, font = 1, cex = 0.5, at = 0.3)
+      axis(side = 2,at = seq(0, 0.3, 0.1),labels = seq(0, 0.3, 0.1), pos = seq(0, 0.3, 0.1), las = 2, lwd = 2, hadj = 0.4)
+      #abline(h = seq(0, 0.3, 0.1),lty=2,lwd=0.3, col = 'gray70')
+      rect(xleft = seq(0, 192, 32), ybottom = -0.05, xright = 192, ytop = -0.02, col = color, border = 'gray70')
+      text(labels = c("C>A","C>G","C>T","T>A","T>C","T>G"),y = rep(-0.08,6),x = seq(0, 192, 32)[2:7]-16, cex = 1)
+    }
+  }
 }
