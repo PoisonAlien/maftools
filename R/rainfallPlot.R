4679f00c9f1f504dfab966089c2be7935d1fb05c
PoisonAlien <anandmt3@gmail.com>
2020-01-24 13:47:00 +0100

    #459 vital and useful PR

--- R/rainfallPlot.R
+++ R/rainfallPlot.R
@@ -117,8 +117,9 @@ rainfallPlot = function(maf, tsb = NULL, detectChangePoints = FALSE,
               pch = 19, xpd = TRUE, xjust = 0, yjust = 0, cex = 1, ncol= 4) #ncol= 2
 
   if(savePlot){
-    pdf(file = paste(tsb, 'rainfallPlot.pdf', sep = '_'),
-                       height = height, width = width, paper = "special", bg = "white")
+    dev.copy( pdf, file = paste(tsb, 'rainfallPlot.pdf', sep = '_'),
+              height = height, width = width, paper = "special", bg = "white")
+    dev.off()
   }
 
 }
