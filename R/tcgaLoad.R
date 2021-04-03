703134d987455c39a5401feb8f2fda80ec51daa4
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-04-03 22:39:38 +0800

    Support loading maf data from gitee repo

--- R/tcgaLoad.R
+++ R/tcgaLoad.R
@@ -1,13 +1,21 @@
 #' Prints available TCGA datasets
 #'
 #' @description Prints available TCGA cohorts
+#' @param repo one of "github" (default) and "gitee" (for users in China).
 #' @examples
 #' tcgaAvailable()
 #' @export
 #' @seealso \code{\link{tcgaLoad}}
-tcgaAvailable = function(){
-  cohorts = data.table::fread(input = "https://github.com/PoisonAlien/TCGAmutations/blob/master/inst/extdata/cohorts.txt?raw=true",
-                              verbose = FALSE,
-                              showProgress = FALSE)
+tcgaAvailable = function(repo = c("github", "gitee")){
+  repo <- match.arg(repo)
+  repo_url <- if (repo == "github") {
+    "https://github.com/PoisonAlien/TCGAmutations/raw/master"
+  } else {
+    "https://gitee.com/ShixiangWang/TCGAmutations/raw/master"
+  }
+  cohorts = data.table::fread(
+    input = paste0(repo_url, "/inst/extdata/cohorts.txt"),
+    verbose = FALSE,
+    showProgress = FALSE)
   cohorts
 }
